8b4baba543debd9d38d97ed2ee5771fe9d2d0f59
Samuel Gomes <SamPongPie@gmail.com>
2024-09-02 23:00:40 -0300

    fix: CSS bug

--- src/assets/components/content/style.jsx
+++ src/assets/components/content/style.jsx
@@ -3,13 +3,13 @@ export const Main = styled.main `
     display:flex;
     width:85%;
     margin:auto;
-    margin-top:100px;
+    margin-top: 70px;
     transform:scale(1.1);
     text-align:center;
     color:white;
 @media only screen and (max-width: 1200px) {
     transform: scale(0.9);
-    margin-top:50px;
+    margin-top:35px;
     width:100%;
 }    
 @media only screen and (max-width: 991px) {
